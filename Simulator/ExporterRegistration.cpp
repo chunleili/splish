145f18748e62abf82c1b0b0f9f26b70416b14821
chunleili <lichunlei1996@126.com>
2022-06-12 18:44:05 +0800

    自定义xyz导出器

--- Simulator/ExporterRegistration.cpp
+++ Simulator/ExporterRegistration.cpp
@@ -5,6 +5,7 @@
 #include "Exporter/RigidBodyExporter_BIN.h"
 #include "Exporter/RigidBodyExporter_OBJ.h"
 #include "Exporter/RigidBodyExporter_VTK.h"
+#include "Exporter/ParticleExporter_xyz.h"
 
 using namespace SPH;
 
@@ -12,8 +13,11 @@ void SimulatorBase::createExporters()
 {
 	addParticleExporter("enablePartioExport", "Partio Exporter", "Enable/disable partio export.", new ParticleExporter_Partio(this));
 	addParticleExporter("enableVTKExport", "VTK Exporter", "Enable/disable VTK export.", new ParticleExporter_VTK(this));
+	addParticleExporter("enableXyzExport", "xyz Exporter", "Enable/disable xyz export.", new ParticleExporter_xyz(this));
+
 
 	addRigidBodyExporter("enableRigidBodyExport", "Rigid Body Exporter", "Enable/disable rigid body BIN export.", new RigidBodyExporter_BIN(this));
 	addRigidBodyExporter("enableRigidBodyOBJExport", "Rigid Body OBJ Exporter", "Enable/disable rigid body OBJ export.", new RigidBodyExporter_OBJ(this));
 	addRigidBodyExporter("enableRigidBodyVTKExport", "Rigid Body VTK Exporter", "Enable/disable rigid body VTK export.", new RigidBodyExporter_VTK(this));
+
 }
