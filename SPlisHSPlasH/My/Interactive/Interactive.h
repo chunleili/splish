48cbe4c2af359e626fa272170b2d3635a775da8d
chunleili <li_cl@foxmail.com>
2022-11-01 18:22:52 +0800

    good key input movement

--- SPlisHSPlasH/My/Interactive/Interactive.h
+++ SPlisHSPlasH/My/Interactive/Interactive.h
@@ -4,8 +4,9 @@
 //用户交互（键盘鼠标）的中介类，用于传递和处理数据
 struct Interactive
 {
-    Vector3r mouse_pos;
+    Vector3r mouse_pos; //鼠标位置
     enum KEY{left=0, right, up, down, forward, backward};
+    Vector3r* p_rb_pos = nullptr; //刚体质心位置的指针
 
     //a singleton method to get the object
     static Interactive& get_inter()
@@ -26,32 +27,67 @@ struct Interactive
     //获取键盘的输入：从GUI\OpenGL\MiniGL.cpp MiniGL::keyboard
     void get_key_input(enum KEY input)
     {
+        const float move_speed = 0.1;
+
         if(input == KEY::down)
-            printf("down!\n");
+        {
+            if(p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[1] -= move_speed;
+                printf("down!\n");
+            }
+        }
         else if(input == KEY::up)
-            printf("up!\n");
+        {
+            if(p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[1] += move_speed;
+                printf("up!\n");
+            }
+        }
         else if(input == KEY::left)
-            printf("left!\n");
-        else if(input == KEY::right)
-            printf("righ!\n");
-        else if(input == KEY::forward)
-            printf("forward!\n");
-        else if(input == KEY::backward)
-            printf("backward!\n");
+        {
+            if(p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[0] -= move_speed;
+                printf("left!\n");
+            }
+        }
+        else if (input == KEY::right)
+        {
+            if (p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[0] += move_speed;
+                printf("right!\n");
+            }
+        }
+        else if (input == KEY::forward)
+        {
+            if (p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[2] -= move_speed;
+                printf("forward!\n");
+            }
+        }
+        else if (input == KEY::backward)
+        {
+            if (p_rb_pos != nullptr)
+            {
+                (*p_rb_pos)[2] += move_speed;
+                printf("backward!\n");
+            }
+        }
     }
 
     //获取刚体的控制权。
     void set_rb_pos(Vector3r& rb_pos)
     {
-        (rb_pos) = mouse_pos; // 获取并设定位置
-        
-        std::cout<< "rb_pos: "<< (rb_pos)<<"\n";
-        
-        // rb_pos[2]=0;
+        // 获取并设定位置为鼠标点击位置 FIXME:
+        // (rb_pos) = mouse_pos; 
 
-        // if(rb_pos[2]<-0.5)
-        //     rb_pos[2] = -.5;
-        // if(rb_pos[2]>0.5)
-        //     rb_pos[2] = .5;
+        //用wasdfb来移动
+        // 获取刚体质心指针
+        p_rb_pos = &rb_pos; 
+        std::cout<< "rb_pos: "<< (rb_pos)<<"\n";
     }
 };
