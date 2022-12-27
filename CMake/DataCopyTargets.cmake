a38b4d4b6e28777d25f118a7d6c2119e08a38919
chunleili <li_cl@foxmail.com>
2022-12-27 22:59:04 +0800

    为共同依赖的第三方target解决冲突问题

--- CMake/DataCopyTargets.cmake
+++ CMake/DataCopyTargets.cmake
@@ -14,14 +14,18 @@ add_custom_target(CopyEmitterModels
 )
 set_target_properties(CopyEmitterModels PROPERTIES FOLDER "Data copy")
 
-add_custom_target(CopyPBDShaders
-	${CMAKE_COMMAND} -E copy_directory
-	${PBD_INCLUDE_DIR}/data/shaders
-	${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/resources/pbd_shaders
-	COMMENT "Copying PBD shaders"
-)
-add_dependencies(CopyPBDShaders Ext_PBD)
-set_target_properties(CopyPBDShaders PROPERTIES FOLDER "Data copy")
+if(NOT TARGET CopyPBDShaders)
+	add_custom_target(CopyPBDShaders
+		${CMAKE_COMMAND} -E copy_directory
+		${PBD_INCLUDE_DIR}/data/shaders
+		${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/resources/pbd_shaders
+		COMMENT "Copying PBD shaders"
+	)
+	add_dependencies(CopyPBDShaders Ext_PBD)
+	set_target_properties(CopyPBDShaders PROPERTIES FOLDER "Data copy")
+else()
+	message(STATUS "CopyPBDShaders target already exists. Skipping creation of CopyPBDShaders target.")
+endif()
 
 add_custom_target(CopyImguiFonts
 	${CMAKE_COMMAND} -E copy
