680ba5e0dc499f5b939372410af6c54388a0d610
chunleili <li_cl@foxmail.com>
2023-01-02 20:04:58 +0800

    修改Discregrid cmake 的Eigen依賴

--- extern/PositionBasedDynamics/Utils/Version.h
+++ extern/PositionBasedDynamics/Utils/Version.h
@@ -5,12 +5,12 @@
 #define STRINGIZE(x) STRINGIZE_HELPER(x)
 #define WARNING(desc) message(__FILE__ "(" STRINGIZE(__LINE__) ") : Warning: " #desc)
 
-#define GIT_SHA1 "0340ab17c69a6ce708c488d6c27c292168f30c3a"
+#define GIT_SHA1 "86de1ec508057c95f947f71ce05da588e3e500da"
 #define GIT_REFSPEC "refs/heads/chunlei-dev"
-#define GIT_LOCAL_STATUS "CLEAN"
+#define GIT_LOCAL_STATUS "DIRTY"
 
 #ifdef DL_OUTPUT
-
+#pragma WARNING(Local changes not committed.)
 #endif
 
 #endif
