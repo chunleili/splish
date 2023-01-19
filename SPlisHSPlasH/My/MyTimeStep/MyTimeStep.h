58c8aac7ee7f6c8d4de39e67e54b39c09b6ec624
chunleili <li_cl@foxmail.com>
2023-01-19 13:47:11 +0800

    增加MyTimeStep(拷贝自DFSPH), 后续修改压力算法将都在此处更改

--- SPlisHSPlasH/My/MyTimeStep/MyTimeStep.h
+++ SPlisHSPlasH/My/MyTimeStep/MyTimeStep.h
@@ -1,28 +1,64 @@
-#ifndef __MyTimeStep_h__
-#define __MyTimeStep_h__
+#pragma once
 
 #include "SPlisHSPlasH/Common.h"
 #include "SPlisHSPlasH/TimeStep.h"
 #include "SimulationDataMyTimeStep.h"
 #include "SPlisHSPlasH/SPHKernels.h"
 
+#define USE_WARMSTART
+#define USE_WARMSTART_V
+
 namespace SPH
 {
 	class SimulationDataMyTimeStep;
+
+	/** \brief 更改自DFSPH
+	*/
 	class MyTimeStep : public TimeStep
 	{
-	private:
-		// SimulationDataMyTimeStep m_simulationData;
-		unsigned int steps=0;
-		void performNeighborhoodSearch();
+	protected:
+		SimulationDataMyTimeStep m_simulationData;
 		unsigned int m_counter;
+		const Real m_eps = static_cast<Real>(1.0e-5);
+		bool m_enableDivergenceSolver;
+		unsigned int m_iterationsV;
+		Real m_maxErrorV;
+		unsigned int m_maxIterationsV;
+
+		void computeDFSPHFactor(const unsigned int fluidModelIndex);
+		void pressureSolve();
+		void pressureSolveIteration(const unsigned int fluidModelIndex, Real &avg_density_err);
+		void divergenceSolve();
+		void divergenceSolveIteration(const unsigned int fluidModelIndex, Real &avg_density_err);
+		void computeDensityAdv(const unsigned int fluidModelIndex, const unsigned int index, const int numParticles, const Real h, const Real density0);
+		void computeDensityChange(const unsigned int fluidModelIndex, const unsigned int index, const Real h);
+
+#ifdef USE_WARMSTART_V
+		void warmstartDivergenceSolve(const unsigned int fluidModelIndex);
+#endif
+#ifdef USE_WARMSTART
+		void warmstartPressureSolve(const unsigned int fluidModelIndex);
+#endif
+
+		/** Perform the neighborhood search for all fluid particles.
+		*/
+		void performNeighborhoodSearch();
+		virtual void emittedParticles(FluidModel *model, const unsigned int startIndex);
+
+		virtual void initParameters();
+
 	public:
+		static int SOLVER_ITERATIONS_V;
+		static int MAX_ITERATIONS_V;
+		static int MAX_ERROR_V;
+		static int USE_DIVERGENCE_SOLVER;
+
 		MyTimeStep();
 		virtual ~MyTimeStep(void);
-		virtual void step() override;
-		virtual void resize() override;
-		virtual void reset() override;
+
+		virtual void step();
+		virtual void reset();
+
+		virtual void resize();
 	};
 }
-
-#endif
