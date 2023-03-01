84d7a49c9d848055ac1882eb775ee9f58dca7c3b
cl <li_cl@foxmail.com>
2023-03-01 21:19:06 +0800

    增加了用户自定义场的mypartio导出，用法:json中指定particleAttributes

--- Simulator/Exporter/ParticleExporter_MyPartio.cpp
+++ Simulator/Exporter/ParticleExporter_MyPartio.cpp
@@ -102,8 +102,125 @@ void ParticleExporter_MyPartio::writeParticlesPartio(const std::string& fileName
         p[1] = x[1];
         p[2] = x[2];
     }
+
+
+	/* -------------------------------------------------------------------------- */
+	/*                               导出splish新增自定义场                        */
+	/* -------------------------------------------------------------------------- */
+	// ADD FEATURE 2023.3.1 : 增加导出splish内部有，但bhclassic中本身没有的量
+	// add attributes
+	std::vector<std::string> attributes;
+	StringTools::tokenize(m_base->getValue<std::string>(SimulatorBase::PARTICLE_EXPORT_ATTRIBUTES), attributes, ";");
+
+	std::cout<<"There are "<<attributes.size()<<" user defined attributes to be exported.\n They are: \n";
+	for(auto attr : attributes)
+	{
+		std::cout<<attr<<"\n";
+	}
+
+	std::cout<<"There are "<<model->numberOfFields()<<" fields in fluid model.\n They are: \n";
+	for (unsigned int j = 0; j < model->numberOfFields(); j++)
+	{	
+		const FieldDescription& field = model->getField(j);
+		std::cout<<field.name<<"\n";
+	}
+
+	std::cout<<"Begin to search and match for user defined attributes in fluid model.\n";
+	// 搜索所有场的名字，看是否和attributes中的名字相同，相同的话，就把场的名字和场的index存入map中，然后为partio中的粒子添加属性
+	std::map<unsigned int, int> attrMap;
+	std::map<unsigned int, Partio::ParticleAttribute> partioAttrMap;
+	for (unsigned int i = 0; i < attributes.size(); i++)
+	{
+		// position is exported anyway
+		if (attributes[i] == "position")
+		{
+			attrMap[i] = -1;
+			continue;
+		}
+		bool found = false;
+		for (unsigned int j = 0; j < model->numberOfFields(); j++)
+		{	
+			const FieldDescription& field = model->getField(j);
+
+			if (field.name == attributes[i])
+			{
+				found = true;
+				std::cout<<"We found attribute "<<attributes[i]<<" in fluid model.\n It's type is: ";
+
+				if (field.type == Scalar)
+				{
+					std::cout<<"Scalar\n";
+					std::cout<<"Adding new attribute "<<attributes[i]<<" to partio.\n";
+					attrMap[i] = j;
+					partioAttrMap[i] = m_particleData->addAttribute(attributes[i].c_str(), Partio::FLOAT, 1);
+				}
+				else if (field.type == UInt)
+				{
+					std::cout<<"UInt\n";
+					attrMap[i] = j;
+					std::cout<<"Adding new attribute "<<attributes[i]<<" to partio.\n";
+					partioAttrMap[i] = m_particleData->addAttribute(attributes[i].c_str(), Partio::INT, 1);
+				}
+				else if (field.type == Vector3)
+				{
+					std::cout<<"Vector3\n";
+					std::cout<<"Adding new attribute "<<attributes[i]<<" to partio.\n";
+					attrMap[i] = j;
+					partioAttrMap[i] = m_particleData->addAttribute(attributes[i].c_str(), Partio::VECTOR, 3);
+				}
+				else
+				{
+					attrMap[i] = -1;
+					LOG_WARN << "Only scalar and vector fields are currently supported by the partio exporter.";
+				}
+				break;
+			}
+		}
+		if (!found)
+		{
+			attrMap[i] = -1;
+			LOG_WARN << "Unknown field cannot be exported in partio file: " << attributes[i];
+		}
+	}
+
+	//然后根据搜索到的属性，写出场
+	for (unsigned int i = 0; i < numParInModel; i++)
+	{
+		for (unsigned int j = 0; j < attributes.size(); j++)
+		{
+			const int fieldIndex = attrMap[j];
+			if (fieldIndex != -1)
+			{
+				const FieldDescription& field = model->getField(fieldIndex);
+				if (field.type == FieldType::Scalar)
+				{
+					float* val = m_particleData->dataWrite<float>(partioAttrMap[j], i);
+					*val = (float)*((Real*)field.getFct(i));
+				}
+				else if (field.type == FieldType::UInt)
+				{
+					int* val = m_particleData->dataWrite<int>(partioAttrMap[j], i);
+					*val = (int)*((unsigned int*)field.getFct(i));
+				}
+				else if (field.type == FieldType::Vector3)
+				{
+					float* val = m_particleData->dataWrite<float>(partioAttrMap[j], i);
+					Eigen::Map<Vector3r> vec((Real*)field.getFct(i));
+					val[0] = (float)vec[0];
+					val[1] = (float)vec[1];
+					val[2] = (float)vec[2];
+				}
+			}
+		}
+	}
+	/* -------------------------------------------------------------------------- */
+	/*                          End of  导出splish新增自定义场                      */
+	/* -------------------------------------------------------------------------- */
+
+
+
     Partio::write(fileName.c_str(), *m_particleData);
-	std::cout << "Write the file: "<<fileName<< std::endl;
+	std::cout << "Write partio data to the file: "<<fileName<< std::endl;
 	print(m_particleData);
 	std::cout << "---------------------\n\n";
 }
